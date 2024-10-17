acc50166a2198eab45b185f5fac4d3e975eee76d
Sam Perry <s.sperry94@gmail.com>
2024-10-17 12:52:51 -0500

    BRANCH:Refactor........renamed dice.py to mainmenu.py.

--- GUI/mainmenu.py
+++ GUI/mainmenu.py
@@ -5,7 +5,7 @@ from tkinter.ttk import Combobox
 
 
 
-class Dice(Tk):
+class MainMenu(Tk):
     def __init__(self, screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = True, sync: bool = False, use: str | None = None) -> None:
         super().__init__(screenName, baseName, className, useTk, sync, use)
         self.geometry("300x240")
@@ -60,6 +60,6 @@ class Dice(Tk):
 
 
 if __name__ == "__main__":
-    app = Dice()
+    app = MainMenu()
     app.mainloop()
 
