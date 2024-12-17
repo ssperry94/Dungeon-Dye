9910e0d8089ed3b51419329b674890ba42c7cacc
Sam Perry <s.sperry94@gmail.com>
2024-12-17 14:32:27 -0600

    BRANCH:QtUi.............got code from .ui file and added it to the MainMenu class. Now making a label with an image.

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ from dungeondye.gui import mainmenu
 if __name__ == "__main__":
     DungeonDye = QtWidgets.QApplication(sys.argv)
     main_menu = mainmenu.MainMenu()
+    main_window = QtWidgets.QMainWindow()
+    main_menu.setupUi(main_window)
     DungeonDye.exec_()
     #DungeonDye.mainloop()
--- src/dungeondye/gui/mainmenu.py
+++ src/dungeondye/gui/mainmenu.py
@@ -1,11 +1,27 @@
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys 
+from PyQt5 import QtCore, QtWidgets
 
-class MainMenu(QtWidgets.QMainWindow):
-    #load in .ui file from QtDesigner
-    #add all other classes into this container
-    def __init__(self):
-        super(MainMenu, self).__init__()
-        uic.loadUi(r'C:\Users\ssper\OneDrive\Projects\Table-Top-Dice-Roller\src\resources\qt\ui\mainwindow.ui', self)
-        self.show()
+class MainMenu(object):
+    def setupUi(self, main_window):
+        #main_window.setObjectName("main_window")
+        main_window.setEnabled(True)
+        main_window.resize(1116, 807)
+        main_window.setAccessibleName("")
+        main_window.setAutoFillBackground(False)
+        main_window.setStyleSheet("""background-color:rgb(35, 40, 48)\n""")
+        main_window.setIconSize(QtCore.QSize(30, 30))
+        main_window.setAnimated(True)
+        main_window.setDockOptions(QtWidgets.QMainWindow.AllowTabbedDocks|QtWidgets.QMainWindow.AnimatedDocks)
+        self.centralwidget = QtWidgets.QWidget(main_window)
+        self.centralwidget.setObjectName("centralwidget")
+        main_window.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(main_window)
+        self.statusbar.setObjectName("statusbar")
+        main_window.setStatusBar(self.statusbar)
+
+        self.retranslateUi(main_window)
+        QtCore.QMetaObject.connectSlotsByName(main_window)
+        main_window.show()
+    def retranslateUi(self, main_window):
+        _translate = QtCore.QCoreApplication.translate
+        main_window.setWindowTitle(_translate("main_window", "Dungeon Dice"))
+        main_window.setToolTip(_translate("main_window", "<html><head/><body><p><br/></p></body></html>"))
