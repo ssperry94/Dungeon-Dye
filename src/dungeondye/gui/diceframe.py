a396077b1d5f3efd2ab4c8d3ba98f56f6897ab9b
Sam Perry <s.sperry94@gmail.com>
2024-12-19 13:18:58 -0600

    BRANCH:QtUi..........created diceframe with code for UI only. working out alignment issues and small bugs now.

--- src/dungeondye/gui/diceframe.py
+++ src/dungeondye/gui/diceframe.py
@@ -1,75 +1,91 @@
-import tkinter as tk 
-from tkinter import ttk
 from dungeondye.gui import roller
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class DiceFrame(tk.Frame):
-        _num_lbl:tk.Label = None
-        _num_combo:ttk.Combobox = None
-        _side_lbl:tk.Label = None
-        _side_combo:ttk.Combobox = None
-        _modifier_lbl:tk.Label = None
-        _modifier_combo:ttk.Combobox = None
-        _outcome_lbl:tk.Label = None
-        _saved_rolls_lbl:tk.Label = None
-        _saved_rolls_combo:ttk.Combobox = None 
-        _roll_btn:tk.Button = None 
+class DiceFrame(QtWidgets.QWidget):
+    _dice_number_label:QtWidgets.QLabel = None 
+    _dice_number_combo:QtWidgets.QComboBox = None 
+    _dice_side_label:QtWidgets.QLabel = None 
+    _dice_side_combo:QtWidgets.QComboBox = None 
+    _modifier_label:QtWidgets.QLabel = None
+    _modifiers_combo:QtWidgets.QComboBox = None 
+    _saved_rolls_label:QtWidgets.QLabel = None 
+    _saved_rolls_combo:QtWidgets.QComboBox = None 
+    roll_button:QtWidgets.QPushButton = None 
+    _output_browser:QtWidgets.QTextBrowser = None 
+    
+    def setupUi(self):
+        self.setObjectName("dice_panel")
+        self.resize(915, 379)
+        self.setStyleSheet("background-color:rgb(35, 40, 48)\n"
+"")
+        self._dice_number_label = QtWidgets.QLabel(self)
+        self._dice_number_label.setGeometry(QtCore.QRect(40, 40, 131, 20))
+        font = QtGui.QFont()
+        font.setFamily("Copperplate Gothic Bold")
+        font.setPointSize(10)
+        self._dice_number_label.setFont(font)
+        self._dice_number_label.setStyleSheet("color:\"#FE2B26\"")
+        self._dice_number_label.setObjectName("_dice_number_label")
+        self._dice_number_combo = QtWidgets.QComboBox(self)
+        self._dice_number_combo.setGeometry(QtCore.QRect(40, 70, 131, 22))
+        self._dice_number_combo.setStyleSheet("color:\"white\"; background-color:\"#6A0DAD\"; border: 1px solid black ")
+        self._dice_number_combo.setEditable(False)
+        self._dice_number_combo.setObjectName("_dice_number_combo")
+        self._dice_side_label = QtWidgets.QLabel(self)
+        self._dice_side_label.setGeometry(QtCore.QRect(40, 100, 131, 20))
+        font = QtGui.QFont()
+        font.setFamily("Copperplate Gothic Bold")
+        font.setPointSize(10)
+        self._dice_side_label.setFont(font)
+        self._dice_side_label.setStyleSheet("color:\"#FE2B26\"")
+        self._dice_side_label.setObjectName("_dice_side_label")
+        self._dice_side_combo = QtWidgets.QComboBox(self)
+        self._dice_side_combo.setGeometry(QtCore.QRect(40, 130, 131, 22))
+        self._dice_side_combo.setStyleSheet("color:\"white\"; background-color:\"#6A0DAD\"; border: 1px solid black ")
+        self._dice_side_combo.setObjectName("_dice_side_combo")
+        self._modifier_label = QtWidgets.QLabel(self)
+        self._modifier_label.setGeometry(QtCore.QRect(40, 160, 131, 20))
+        font = QtGui.QFont()
+        font.setFamily("Copperplate Gothic Bold")
+        font.setPointSize(10)
+        self._modifier_label.setFont(font)
+        self._modifier_label.setStyleSheet("color:\"#FE2B26\"")
+        self._modifier_label.setObjectName("_modifier_label")
+        self._modifiers_combo = QtWidgets.QComboBox(self)
+        self._modifiers_combo.setGeometry(QtCore.QRect(40, 180, 131, 22))
+        self._modifiers_combo.setStyleSheet("color:\"white\"; background-color:\"#6A0DAD\"; border: 1px solid black ")
+        self._modifiers_combo.setObjectName("_modifiers_combo")
+        self.roll_button = QtWidgets.QPushButton(self)
+        self.roll_button.setGeometry(QtCore.QRect(450, 220, 93, 28))
+        self.roll_button.setStyleSheet("background-color:\"#FE2B26\"")
+        self.roll_button.setText("")
+        self.roll_button.setObjectName("roll_button")
+        self._saved_roll_combo = QtWidgets.QComboBox(self)
+        self._saved_roll_combo.setGeometry(QtCore.QRect(780, 140, 131, 22))
+        self._saved_roll_combo.setStyleSheet("color:\"white\"; background-color:\"#6A0DAD\"; border: 1px solid black ")
+        self._saved_roll_combo.setObjectName("_saved_roll_combo")
+        self._saved_rolls_label = QtWidgets.QLabel(self)
+        self._saved_rolls_label.setGeometry(QtCore.QRect(780, 110, 131, 20))
+        font = QtGui.QFont()
+        font.setFamily("Copperplate Gothic Bold")
+        font.setPointSize(10)
+        self._saved_rolls_label.setFont(font)
+        self._saved_rolls_label.setStyleSheet("color:\"#FE2B26\"")
+        self._saved_rolls_label.setObjectName("_saved_rolls_label")
+        self._output_browser = QtWidgets.QTextBrowser(self)
+        self._output_browser.setGeometry(QtCore.QRect(300, 260, 381, 131))
+        self._output_browser.setStyleSheet("color:\"#6A0DAD\";border:1px solid white;")
+        self._output_browser.setObjectName("_output_browser")
 
-        def __init__(self, parent):
-                super().__init__(parent, highlightbackground= "black", highlightthickness = 1)
-                #self.config(bg = "brown")
-                self.grid(column = 0, row = 1)
-                self._create_widgets()
-                self._create_buttons()
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
 
-            #wrapper function that instantiates a Dice object and modifies the outcome label with the final roll
-        def _roll_dice(self) -> None:
-                dye = roller.Dice()
-                dye.rolldice(self._outcome_lbl, self._num_combo, self._side_combo, self._modifier_combo)
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("dice_panel", "Form"))
+        self._dice_number_label.setText(_translate("dice_panel", "Dice Number"))
+        self._dice_side_label.setText(_translate("dice_panel", "Dice Side"))
+        self._modifier_label.setText(_translate("dice_panel", "Modifiers "))
+        self._saved_rolls_label.setText(_translate("dice_panel", "Saved Rolls"))
 
-        def _create_widgets(self):
-                self._num_lbl = tk.Label(self, text = "Dice Number", bg = "white", fg = "green")
-                self._num_lbl.grid(row = 1, column = 0, sticky = "nsew")
-
-                #numlist - range for dice number combolist
-                #combo box that simulates how many dice to roll
-                numlist: list[int] = [num for num in range(1,101)]
-                self._num_combo = ttk.Combobox(self)
-                self._num_combo['values'] = numlist
-                self._num_combo.grid(row = 2, column = 0)
-
-                #tk.Label that goes above the combo box for dice side 
-                self._side_lbl = tk.Label(self, text = "Dice Side", bg = "white", fg = "green")
-                self._side_lbl.grid(row = 3, column = 0)
-
-                #combo box to select the dice side 
-                self._side_combo = ttk.Combobox(self)
-                self._side_combo['values'] = (4,6,8,10,12,20,100)
-                self._side_combo.grid(row = 4, column = 0)
-
-                #tk.Label above modifer combo box
-                #mod_list holds all options for adding some modifier 
-                mod_list: list[int] = [num for num in range(0,101)]
-                self._modifier_lbl = tk.Label(self, text = "Modifier number",bg = "white", fg = "green")
-                self._modifier_lbl.grid(row = 5, column = 0)
-
-                #adds a modifer to the roll
-                self._modifier_combo = ttk.Combobox(self)
-                self._modifier_combo['values'] = mod_list
-                self._modifier_combo.current(0)
-                self._modifier_combo.grid(row = 6, column = 0)
-
-                #displays the outcome of the roll
-                self._outcome_lbl = tk.Label(self,bg = "white", fg = "green")
-                self._outcome_lbl.grid(row = 8, column = 1)
-
-                self._saved_rolls_lbl = tk.Label(self, text = "My Dice", bg = "white", fg = "green")
-                self._saved_rolls_lbl.grid(row = 4, column = 2)
-
-                self._saved_rolls_combo = ttk.Combobox(self)
-                #add combox list here
-                self._saved_rolls_combo.grid(row = 5, column = 2)
-
-        def _create_buttons(self) -> None:
-                #button that rolls the dice
-                self._roll_btn = tk.Button(self, text = "Roll", command = lambda: self._roll_dice())
-                self._roll_btn.grid(row = 7, column = 1, padx = 50, pady = 15)
--- src/dungeondye/gui/mainmenu.py
+++ src/dungeondye/gui/mainmenu.py
@@ -1,26 +1,32 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
+from dungeondye.gui import diceframe
 
 class MainMenu(object):
     _title_label:QtWidgets.QLabel
     _pixmap:QtGui.QPixmap
+    _layout:QtWidgets.QVBoxLayout 
 
     def setupUi(self, main_window:QtWidgets.QMainWindow) -> None:
-        #main_window.setObjectName("main_window")
+        main_window.setObjectName("main_window")
         main_window.setEnabled(True)
-        main_window.resize(1116, 807)
+        main_window.resize(1250, 900)
         main_window.setAccessibleName("")
         main_window.setAutoFillBackground(False)
         main_window.setStyleSheet("""background-color:rgb(35, 40, 48)\n""")
         main_window.setIconSize(QtCore.QSize(30, 30))
         main_window.setAnimated(True)
         main_window.setDockOptions(QtWidgets.QMainWindow.AllowTabbedDocks|QtWidgets.QMainWindow.AnimatedDocks)
+        
         self.centralwidget = QtWidgets.QWidget(main_window)
         self.centralwidget.setObjectName("centralwidget")
+        self._layout = QtWidgets.QVBoxLayout(self.centralwidget)
+        # self._layout.setContentsMargins(0,0,0,0)
         main_window.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(main_window)
         self.statusbar.setObjectName("statusbar")
         main_window.setStatusBar(self.statusbar)
         self._initalize_widgets()
+        self._initalize_frames(main_window)
         self.retranslateUi(main_window)
         QtCore.QMetaObject.connectSlotsByName(main_window)
         main_window.show()
@@ -36,9 +42,18 @@ class MainMenu(object):
 
         #create title label and add logo
         self._title_label = QtWidgets.QLabel(self.centralwidget)
-        self._title_label.setGeometry(QtCore.QRect(390, 30, 331, 221))
+        self._layout.addWidget(self._title_label, alignment=QtCore.Qt.AlignCenter)
+        #self._title_label.setGeometry(QtCore.QRect(390, 30, 331, 221))
+        self._title_label.setFixedSize(331,221)
         self._title_label.setPixmap(pixmap)
         self._title_label.setStyleSheet("border: 1px solid black")
         self._title_label.setScaledContents(True)
-        self._title_label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        #self._title_label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self._title_label.setObjectName("title_label")
+        
+    
+    def _initalize_frames(self, main_window:QtWidgets.QMainWindow):
+        dice_frame = diceframe.DiceFrame(main_window)
+        dice_frame.setupUi()
+        self._layout.addWidget(dice_frame)
+
