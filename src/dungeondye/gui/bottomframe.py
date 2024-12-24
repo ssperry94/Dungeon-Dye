eb2a8a7ddd309ff60630791e8b9e18fd5ed0fef1
Sam Perry <s.sperry94@gmail.com>
2024-12-24 13:01:42 -0600

    BRANCH:QtUi.......added better spacing and better button size in ButtonFrame.

--- src/dungeondye/gui/bottomframe.py
+++ src/dungeondye/gui/bottomframe.py
@@ -5,6 +5,9 @@ class ButtonFrame(QtWidgets.QWidget):
     _save_button:QtWidgets.QPushButton
     _settings_button:QtWidgets.QPushButton
     _frame_layout:QtWidgets.QHBoxLayout
+    _button_height:int = 85
+    _button_width:int = 23
+
 
     def setupUI(self) -> None:
         self.setObjectName("button_panel")
@@ -12,23 +15,24 @@ class ButtonFrame(QtWidgets.QWidget):
 "")
         self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self._frame_layout = QtWidgets.QHBoxLayout(self)
+        self._frame_layout.setSpacing(100)
         self._initalize_buttons()
 
     def _initalize_buttons(self) -> None:
         self._exit_button = QtWidgets.QPushButton(text = "Exit")
         self._exit_button.setStyleSheet("background-color:\"#FE2B26\"")
         self._exit_button.setObjectName("exit_button")
-        self._exit_button.setFixedSize(93,28)
+        self._exit_button.setFixedSize(self._button_height, self._button_width)
         self._frame_layout.addWidget(self._exit_button)
 
         self._save_button = QtWidgets.QPushButton(text = "Save")
         self._save_button.setStyleSheet("background-color:\"#FE2B26\"")
         self._save_button.setObjectName("save_button")
-        self._save_button.setFixedSize(93,28)
+        self._save_button.setFixedSize(self._button_height, self._button_width)
         self._frame_layout.addWidget(self._save_button)
 
         self._settings_button = QtWidgets.QPushButton(text = "Settings")
         self._settings_button.setStyleSheet("background-color:\"#FE2B26\"")
         self._settings_button.setObjectName("settings_button")
-        self._settings_button.setFixedSize(93,28)
+        self._settings_button.setFixedSize(self._button_height, self._button_width)
         self._frame_layout.addWidget(self._settings_button)
