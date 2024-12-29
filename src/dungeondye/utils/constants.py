26d5866f7a26e07769281ae898c493aa923b4059
Sam Perry <s.sperry94@gmail.com>
2024-12-28 18:02:15 -0600

    BRANCH:RollSaver..........added a constants.py for all constant values.

---
+++ src/dungeondye/utils/constants.py
@@ -0,0 +1,6 @@
+'''
+File containing all constatns and settings read in from settings.json
+Need functionality for settings.json as well
+'''
+
+SAVED_ROLLS_PATH:str = r'C:\Users\ssper\OneDrive\Projects\Table-Top-Dice-Roller\src\resources\jsons\saved_rolls.json'
